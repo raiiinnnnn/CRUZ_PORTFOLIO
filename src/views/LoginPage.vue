af9a1d227151b6abaf8ee4779f907c4daf90c732
raiiinnnnn <2405.rainielcruz@gmail.com>
2024-11-29 19:53:07 +0800

    Update LoginPage.vue

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -68,7 +68,7 @@ export default {
       if (this.email === validEmail && this.password === validPassword) {
         alert('Login successful!');
         // Redirect to the specified URL
-        window.location.href = 'https://cruz-portfolio-theta.vercel.app/portfolio/profile';
+        window.location.href = 'https://cruz-portfolio-jade.vercel.app/login';
       } else {
         this.errorMessage = 'Invalid email or password. Please try again.';
       }
